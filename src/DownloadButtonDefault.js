5026b5c517a784325a010c5a1b38a91a6f933e3c
Talha <talhabintariq@gmail.com>
2021-09-07 23:34:01 -0400

    WIP

---
+++ src/DownloadButtonDefault.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./DownloadButtonDefault.css";
+
+function DownloadButtonDefault() {
+  return (
+    <div className="default">
+      <h1>Paste the video url to get started..</h1>
+    </div>
+  );
+}
+
+export default DownloadButtonDefault;
---
+++ src/DownloadButtonInvalid.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DownloadButtonInvalid() {
+  return (
+    <div style={{ margin: "20px" }}>
+      <h1 style={{ color: "red" }}>Invalid url</h1>
+    </div>
+  );
+}
+
+export default DownloadButtonInvalid;
